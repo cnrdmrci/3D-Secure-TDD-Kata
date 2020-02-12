52d70f77c7eaaf4f9a45a8a08dd3ec7ac4be0ca2
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-12 23:11:55 +0300

    project created

---
+++ 3DSecureKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3DSecureKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
